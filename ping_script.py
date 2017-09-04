79a0bfdf24f777440d65fc352b53ad4febbf0e91
sobiya <sobiyasardar@gmail.com>
2017-09-04 22:00:10 +0530

    Python Script for small network

--- ping_script.py
+++ ping_script.py
@@ -4,13 +4,10 @@ import time
 for i in range(101,106):
 	ip="192.168.0."+str(i)
 	m=time.strftime('%Y-%m-%d %I:%M:%S')
-	#print m
 	res=subprocess.call(['ping','-c','3',ip])
 	if res==0:
 		db = MySQLdb.connect(host="localhost", user="root",passwd="root",db="osproject")
 		cur = db.cursor()
-		cur.execute("INSERT INTO os_table (ip,pc_no,status,last_update)values(%s,%s,%s,%s)",(ip,i,res,m))
+		cur.execute("INSERT INTO os_table (ip,pc_no,status,last_update)values(%s,%s,%s,%s)",(ip,i,"ON",m))
 		db.commit()
 		db.close()
-	#else:
-		#print "OK"
