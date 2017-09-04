0105c5796b29c9c7e955ee5098c81ab858e8cd08
sobiya <sobiyasardar@gmail.com>
2017-09-04 21:31:19 +0530

    Final API file To fetch data from Mysql

--- mysql_connect_retrieve.php
+++ mysql_connect_retrieve.php
@@ -1,24 +1,23 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "osproject";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$sql = "SELECT * FROM os_table";
-$result=mysqli_query($conn,$sql);
-
-$response=array();
-
-while($row=mysqli_fetch_array($result)){
-	array_push($response, array("kid"=>$row[0],"kip"=>$row[1],"kpc_no"=>$row[2],"kstatus"=>$row[3],"klast_update"=>$row[4]));
-}
-echo json_encode(array("server_response"=>$response));
-mysqli_close($conn);
-?>
+ $servername = "localhost";
+ $username = "root";
+ $password = "root";
+ $dbname = "osproject";
+ 
+ // Create connection
+ $conn = new mysqli($servername, $username, $password,$dbname);
+ 
+ // Check connection
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+ $sql = "SELECT * FROM os_table";
+ $query=mysqli_query($conn,$sql);
+ 
+ if($query){
+ 	while($row=mysqli_fetch_array($query,MYSQLI_ASSOC)){
+ 	$r[]=$row;
+ 	}
+ }
+ echo json_encode($r);
+ ?> 
