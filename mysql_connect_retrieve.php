3416c8d63958ca092021264917bdd838fb6ebe85
sobiya <sobiyasardar@gmail.com>
2017-09-05 00:25:09 +0530

    Final API to get data from mysql in 10 minutes

--- mysql_connect_retrieve.php
+++ mysql_connect_retrieve.php
@@ -11,9 +11,10 @@
  if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
  }
- $sql = "SELECT * FROM os_table";
+ $sql = "SELECT * FROM os_table WHERE DATE_ADD(last_update, INTERVAL 10 MINUTE) >= NOW()";
  $query=mysqli_query($conn,$sql);
- 
+
+
  if($query){
  	while($row=mysqli_fetch_array($query,MYSQLI_ASSOC)){
  	$r[]=$row;
